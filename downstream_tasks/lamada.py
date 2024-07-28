cb5e8f26247a79d8d5aaf3687d7c0ec72a9c03af
root <root@fair-song-crunches-fin-01.datacrunch.io>
2024-07-28 11:37:17 +0000

    update lamaba benchmark

--- downstream_tasks/lamada.py
+++ downstream_tasks/lamada.py
@@ -2,21 +2,21 @@ from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import torch.nn as nn
+from tqdm import tqdm
 
 def prepare_dataset():
     ds = load_dataset("cimec/lambada", split='test')
     return ds
 
-if __name__=='main':
+if __name__ == '__main__':
     dataset = prepare_dataset()
-    breakpoint()
-    model_id = 'EleutherAI/pythia-1.4b'
+    model_id = 'google/recurrentgemma-2b'
     model = AutoModelForCausalLM.from_pretrained(model_id).to("cuda")
     tokenizer = AutoTokenizer.from_pretrained(model_id)
-
+    correct = 0
     # Tokenize input and prepare for model
-    for entry in ds:
-        input_ids = tokenizer.encode(entry, return_tensors='pt')
+    for entry in tqdm(dataset):
+        input_ids = tokenizer.encode(entry['text'], return_tensors='pt').to("cuda")
         input_len = len(input_ids[0])
 
         # Generate model output
@@ -25,5 +25,9 @@ if __name__=='main':
             predictions = outputs.logits
 
         # Get the predicted word
-        predicted_index = torch.argmax(predictions[0, -1, :]).item()
-        predicted_word = tokenizer.decode([predicted_index])
+        predicted_index = torch.argmax(predictions[0, -2, :]).item()
+        ground_truth = input_ids[:, -1].item()
+        if ground_truth == predicted_index:
+            correct += 1
+    
+    print(correct / len(dataset))
